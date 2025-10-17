464c185c765c7695f9adc46ffa55a58b7c3a98f5
JasonSunnnn <77427638+JasonSunnnn@users.noreply.github.com>
2025-10-17 23:08:00 +0800

    Add files via upload

---
+++ main.m
@@ -0,0 +1,24 @@
+clear;
+clc;
+close all;
+%%                                    Load Data
+load('indicators.mat')
+
+Prob_set={'WFG1','WFG2','WFG3','WFG4','WFG5','WFG6','WFG7','WFG8','WFG9'}'; %Test problem set, corresponding to the column of the indicator cell
+M_set=[2,3,5,8,10];  %Set of objective numbers, corresponding to rows of indicator cells
+Algo_set={'SPEA2','NSGAII','GrEA','NSGAIII','SPEA2SDE','KnEA','tDEA','VaEA','SPEAR',...
+    'MOEAD','MOEADDE','IDBEA','MOEADD','MaOEARD','EFRRR','RVEA',... 
+    'IBEA','HypE','BiGE','MOMBIII','BCEIBEA'};
+
+%%                                          Model Evaluation
+Score=cell(5,9);
+ 
+for i=1:9                                 
+    for ii=1:5                            
+        for iii=1:10                       
+            Homogenous_set=[powerconsum{ii,i}(:,iii),data_PD_31{ii,i}(:,iii),data_NFS{ii,i}(:,iii),data_HV_96{ii,i}(:,iii),data_GDPlus{ii,i}(:,iii),data_Spacing_39{ii,i}(:,iii)];            
+            a=EvaluationModel(Homogenous_set(:,1),Homogenous_set(:,2:4),Homogenous_set(:,5:6));  
+            Score{ii,i}(:,iii)=a;
+        end
+    end
+end
