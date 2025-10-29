a19e193034f1cba39eb934f70f3f41608bde5d84
JasonSunnnn <77427638+JasonSunnnn@users.noreply.github.com>
2025-10-29 09:41:14 +0800

    Add files via upload

--- EvaluationModel.m
+++ EvaluationModel.m
@@ -1,43 +1,37 @@
-function [output]=EvaluationModel(X,Y,V)         %  X, Y, and V represent the input matrix, 
-% desirable output matrix, and undesirable output matrix, respectively. 
+function [output]=EvaluationModel(Y,V)         %  Y and V represent the desirable output matrix and undesirable output matrix, respectively. 
 % Each row corresponds to a Decision Making Unit (DMU), and each column corresponds to a different metric.   
-n=size(X,1);
-nn_X=size(X,2);nn_Y=size(Y,2);nn_V=size(V,2);
+n=size(Y,1);
+nn_Y=size(Y,2);nn_V=size(V,2);
 for k=1:n
-     c=[1;zeros(2*n,1);-[(1/2*nn_V)*(1./V(k,:)')];-[(1/2*nn_X)*(1./X(k,:)')];zeros(nn_Y,1)];
-     A=[X(k,:)',-X',zeros(nn_X,n),zeros(nn_X,nn_V+nn_X+nn_Y);
-        -V(k,:)',V',zeros(nn_V,n),diag(ones(nn_V,1)),zeros(nn_V,nn_X+nn_Y);    
-        -X(k,:)',zeros(nn_X,n),X',zeros(nn_X,nn_V),diag(ones(nn_X,1)),zeros(nn_X,nn_Y);
-        Y(k,:)',zeros(nn_Y,n),-Y',zeros(nn_Y,nn_V+nn_X),diag(ones(nn_Y,1))];    
-     b=[zeros(nn_X,1);      
-        zeros(nn_V,1);                                            
-        zeros(nn_X,1);
+     c=[1;zeros(2*n,1);-[(1/nn_V)*(1./V(k,:)')];zeros(nn_Y,1)];
+     A=[-V(k,:)',V',zeros(nn_V,n),diag(ones(nn_V,1)),zeros(nn_V,nn_Y);    
+        Y(k,:)',zeros(nn_Y,n),-Y',zeros(nn_Y,nn_V),diag(ones(nn_Y,1))];    
+     b=[zeros(nn_V,1);                                            
         zeros(nn_Y,1);]                                           
-     Aeq=[1,zeros(1,2*n),zeros(1,nn_V+nn_X),(nn_Y)*(1./Y(k,:));   
-         -1,ones(1,n),zeros(1,n),zeros(1,nn_V+nn_X+nn_Y);
-         -1,zeros(1,n),ones(1,n),zeros(1,nn_V+nn_X+nn_Y);
+     Aeq=[1,zeros(1,2*n),zeros(1,nn_V),(nn_Y)*(1./Y(k,:));   
+         -1,ones(1,n),zeros(1,n),zeros(1,nn_V+nn_Y);
+         -1,zeros(1,n),ones(1,n),zeros(1,nn_V+nn_Y);
          ];
      beq=[1;0;0];
-     lb=[zeros(2*n+1+nn_V+nn_X+nn_Y,1);];
+     lb=[zeros(2*n+1+nn_V+nn_Y,1);];
      ub=[];
+     % options = optimoptions('linprog', 'TolFun', 1e-10);
      W(:,k)=cplexlp(c,A,b,Aeq,beq,lb,ub);
 end
      t=W(1,:);
-     Slack_solution=W(2*n+2:2*n+1+nn_V+nn_X+nn_Y,:);
+     Slack_solution=W(2*n+2:2*n+1+nn_V+nn_Y,:);
      Slack_original=Slack_solution./t;    
-     Matrix=[V,X,Y]';
+     Matrix=[V,Y]';
      Corresponding_division=Slack_original./Matrix;
 
      Vslack=Corresponding_division(1:nn_V,:);
-     Xslack=Corresponding_division(nn_V+1:nn_V+nn_X,:);
-     Yslack=Corresponding_division(nn_V+nn_X+1:nn_V+nn_X+nn_Y,:);
+     Yslack=Corresponding_division(nn_V+1:nn_V+nn_Y,:);
 
-     X_cofficient=mean(Xslack,1); X_cofficient=X_cofficient./2;
-     V_cofficient=mean(Vslack,1);  V_cofficient=V_cofficient./2;
+     V_cofficient=mean(Vslack,1);
      Y_cofficient=mean(Yslack,1);
 
-     Numerator=1.-(V_cofficient + X_cofficient);
+     Numerator=1.-V_cofficient ;
      Denominator=1.+Y_cofficient;
      L=Numerator./Denominator;     
      [output]=L';
-end                              
+end                                         
--- main.m
+++ main.m
@@ -10,15 +10,35 @@ Algo_set={'SPEA2','NSGAII','GrEA','NSGAIII','SPEA2SDE','KnEA','tDEA','VaEA','SPE
     'MOEAD','MOEADDE','IDBEA','MOEADD','MaOEARD','EFRRR','RVEA',... 
     'IBEA','HypE','BiGE','MOMBIII','BCEIBEA'};
 
+%%                                  Flat Data
+cellArray = coverage;
+
+flatData = cellfun(@(x) x(:), cellArray, 'UniformOutput', false);
+
+resultMatrix = vertcat(flatData{:});
+
+disp(resultMatrix);
+
+disp(size(resultMatrix));  
+
+coverage_flat=resultMatrix;          
+
+%%                                   Calculate Correlation
+
+array1=coverage_flat;
+array2=array1;
+
+[tau, p_value] = corr(array1, array2,'Type', 'Kendall');
+
+
 %%                                          Model Evaluation
-Score=cell(5,9);
- 
-for i=1:9                                 
-    for ii=1:5                            
-        for iii=1:10                       
-            Homogenous_set=[powerconsum{ii,i}(:,iii),data_PD_31{ii,i}(:,iii),data_NFS{ii,i}(:,iii),data_HV_96{ii,i}(:,iii),data_GDPlus{ii,i}(:,iii),data_Spacing_39{ii,i}(:,iii)];            
-            a=EvaluationModel(Homogenous_set(:,1),Homogenous_set(:,2:4),Homogenous_set(:,5:6));  
-            Score{ii,i}(:,iii)=a;
+score=cell(5,9);
+for i=1:9                                   %   闂
+    for ii=1:5                              %   鐩爣
+        for iii=1:10                        %   閲嶅瀹為獙娆℃暟
+            Homogenous_set=[data_PD_31{ii,i}(:,iii),data_NFS{ii,i}(:,iii),data_HV_96{ii,i}(:,iii),data_GDPlus{ii,i}(:,iii),data_Spacing_39{ii,i}(:,iii)];                    %   鍒涘缓涓?涓寘鍚悓璐ㄥ崟鍏冪殑鏁版嵁缁?
+            a=EvaluationModel(Homogenous_set(:,1:3),Homogenous_set(:,4:4));    %  绠楁晥鐜?
+            score{ii,i}(:,iii)=a;
         end
     end
 end
